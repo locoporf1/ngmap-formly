5bf50e9b75508d3171f118105af59d949a3ada00
m.rodriguez.tena <m.rodriguez.tena@CPX-UOMINE2ZYEB.dir.svc.accenture.com>
2016-11-16 11:38:27 +0100

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 
 gulp.task('default', function () {
     return gulp.src('./src/*.js')
-    .pipe(concat('ngmap-formly.js'))
     .pipe(gulp.dest('./dist/'));
 });
